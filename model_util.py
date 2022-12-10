2858c047984633ff3e5cbf88276da9124791b38b
Chunhui Gu <chunhui.gu@alumni.emory.edu>
2022-12-10 16:09:53 -0600

    save

--- model_util.py
+++ model_util.py
@@ -106,6 +106,38 @@ def get_optimizer(learning_rate):
   return optimizer
 
 
+# def linear_layer(x,
+#                  is_training,
+#                  num_classes,
+#                  use_bias=True,
+#                  use_bn=False,
+#                  name='linear_layer'):
+#   """Linear head for linear evaluation.
+#
+#   Args:
+#     x: hidden state tensor of shape (bsz, dim).
+#     is_training: boolean indicator for training or test.
+#     num_classes: number of classes.
+#     use_bias: whether or not to use bias.
+#     use_bn: whether or not to use BN for output units.
+#     name: the name for variable scope.
+#
+#   Returns:
+#     logits of shape (bsz, num_classes)
+#   """
+#   assert x.shape.ndims == 2, x.shape
+#   with tf.variable_scope(name, reuse=tf.AUTO_REUSE):
+#     x = tf.layers.dense(
+#         inputs=x,
+#         units=num_classes,
+#         use_bias=use_bias and not use_bn,
+#         kernel_initializer=tf.random_normal_initializer(stddev=.01))
+#     if use_bn:
+#       x = resnet.batch_norm_relu(x, is_training, relu=False, center=use_bias)
+#     x = tf.identity(x, '%s_out' % name)
+#   return x
+
+
 def linear_layer(x,
                  is_training,
                  num_classes,
@@ -129,13 +161,23 @@ def linear_layer(x,
   with tf.variable_scope(name, reuse=tf.AUTO_REUSE):
     x = tf.layers.dense(
         inputs=x,
-        units=num_classes,
+        units=100,
         use_bias=use_bias and not use_bn,
         kernel_initializer=tf.random_normal_initializer(stddev=.01))
     if use_bn:
       x = resnet.batch_norm_relu(x, is_training, relu=False, center=use_bias)
+
+    
+    x = tf.layers.dense(
+        inputs=x,
+        units=num_classes,
+        use_bias=use_bias and not use_bn,
+        kernel_initializer=tf.random_normal_initializer(stddev=.01))
     x = tf.identity(x, '%s_out' % name)
   return x
+
+
+
 
 
 def projection_head(hiddens, is_training, name='head_contrastive'):
