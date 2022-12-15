update no batch normalization bug

data.py
@@ -130,17 +130,17 @@ def build_input_fn(builder, is_training):
         label = tf.one_hot(label, num_classes)
       return image, label, 1.0
 
-      # split flag for which part of data to use
+    # split flag for which part of data to use
     split = FLAGS.train_split if is_training else FLAGS.eval_split
     # for example train[:80%] will only use the first 80% data in the train split
     if FLAGS.train_split_perc != 100 and is_training:
       split = split + '[:' + str(FLAGS.train_split_perc) + '%]'
 
     dataset = builder.as_dataset(
-      split=split,
-      shuffle_files=is_training, as_supervised=True)
-    # if FLAGS.cache_dataset:
-    #   dataset = dataset.cache()
+        split=FLAGS.train_split if is_training else FLAGS.eval_split,
+        shuffle_files=is_training, as_supervised=True)
+    if FLAGS.cache_dataset:
+      dataset = dataset.cache()
     if is_training:
       buffer_multiplier = 50 if FLAGS.image_size <= 32 else 10
       dataset = dataset.shuffle(params['batch_size'] * buffer_multiplier)

model_util.py
@@ -134,7 +134,7 @@ def linear_layer(x,
         kernel_initializer=tf.random_normal_initializer(stddev=.01))
     if use_bn:
       x = resnet.batch_norm_relu(x, is_training, relu=False, center=use_bias)
-
+    x = tf.identity(x, '%s_out' % name)
   return x
 
 

run.py
@@ -37,14 +37,10 @@ import tensorflow_hub as hub
 
 FLAGS = flags.FLAGS
 
+
 flags.DEFINE_integer(
     'train_split_perc', 100,
     'Default use 100% of the training example')
-
-
-flags.DEFINE_enum(
-    'classifier', 'linear', ['linear', 'non-linear'],
-    'Use a linear or non-linear classifier head otherwise a linear classifier head')
 
 
 flags.DEFINE_float(
@@ -377,8 +373,6 @@ def main(argv):
     tf.config.set_soft_device_placement(True)
 
 
-
-
   builder = tfds.builder(FLAGS.dataset, data_dir=FLAGS.data_dir)
   builder.download_and_prepare()
   num_train_examples = builder.info.splits[FLAGS.train_split].num_examples
@@ -386,13 +380,8 @@ def main(argv):
   num_classes = builder.info.features['label'].num_classes
 
   train_steps = model_util.get_train_steps(num_train_examples)
-  # recalculate the training steps if only use a portion of training set
-  # cau
-  if FLAGS.train_split_perc != 100:
-    train_steps = model_util.get_train_steps(num_train_examples * FLAGS.train_split_perc/100)
   eval_steps = int(math.ceil(num_eval_examples / FLAGS.eval_batch_size))
   epoch_steps = int(round(num_train_examples / FLAGS.train_batch_size))
-
 
   resnet.BATCH_NORM_DECAY = FLAGS.batch_norm_decay
   model = resnet.resnet_v1(
@@ -439,7 +428,7 @@ def main(argv):
       try:
         result = perform_evaluation(
             estimator=estimator,
-            input_fn=data_lib.build_input_fn(False),
+            input_fn=data_lib.build_input_fn(builder, False),
             eval_steps=eval_steps,
             model=model,
             num_classes=num_classes,
@@ -450,7 +439,7 @@ def main(argv):
         return
   else:
     estimator.train(
-      data_lib.build_input_fn(builder, True), max_steps=train_steps)
+        data_lib.build_input_fn(builder, True), max_steps=train_steps)
     if FLAGS.mode == 'train_then_eval':
       perform_evaluation(
           estimator=estimator,